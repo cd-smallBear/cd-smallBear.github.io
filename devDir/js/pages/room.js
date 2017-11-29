504291bc40248e2efb656adf1219b45c1178f9b7
wangxiong <wangxiong@cms.com>
2017-11-29 14:19:25 +0800

    done : postMessage

--- devDir/js/pages/room.js
+++ devDir/js/pages/room.js
@@ -1,16 +1,4 @@
 require.config(Hotel.requireConfig);
-// setTimeout(function(){
-
-//   window.frames[0].postMessage({
-//     name :"smallbear"
-//   },"https://cd-smallbear.github.io");
-// },2000)
-var fn = function(e){
-    console.log(e.data);
-    e.source.postMessage("I Am Bear",e.origin);
-    window.removeEventListener("message",fn);
-};
-window.addEventListener("message",fn);
 define(["ajaxSub","dropdownSelect","bsDialog","webuploader.setting"],function(ajaxSub,dropdownSelect,bsDialog,upload){
     
     dropdownSelect($("#search"));
